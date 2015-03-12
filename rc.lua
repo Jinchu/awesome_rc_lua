d6469fa0f96f07d5467eb621bacc266cdf0a3550
Panu Simolin <p.simolin@gmail.com>
2015-03-12 21:04:15 +0100

    added nm-applet to launch. Seems to be working fine. Should not launch second one, when one instance is already running

--- rc.lua
+++ rc.lua
@@ -450,6 +450,8 @@ client.connect_signal("manage", function (c, startup)
     end
 end)
 
+awful.util.spawn_with_shell("pgrep -u $USER -x nm-applet > /dev/null || (nm-applet &)")
+
 client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 -- }}}
