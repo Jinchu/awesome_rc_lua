8773f9ddc580b87a15b395bd462555c20acb36d0
Panu Simolin <p.simolin@gmail.com>
2015-03-16 15:41:39 +0100

    added power management spawn. Seems working well.

--- rc.lua
+++ rc.lua
@@ -451,6 +451,7 @@ client.connect_signal("manage", function (c, startup)
 end)
 
 awful.util.spawn_with_shell("pgrep -u $USER -x nm-applet > /dev/null || (nm-applet &)")
+awful.util.spawn_with_shell("xfce4-power-manager")
 
 client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
